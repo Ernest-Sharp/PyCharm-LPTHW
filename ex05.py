916b100f4b785bbdb677779f6df191bb7b366c43
Ernest-Sharp <es29192@email.vccs.eu>
2016-05-12 13:38:20 -0500

    Más cambios en LPTHW Ejercicios 5, 6 y 7

---
+++ ex05.py
@@ -0,0 +1,30 @@
+# -- coding: utf-8 --
+my_name = "Zed A. Shaw"
+my_age = 35  # not a lie
+my_height = 74  # inches
+my_weight = 180  # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+# Conversión a sistema métrico
+# Pulgadas a centímetros 1" 2.54cm
+# Libras a kilos 1lb 0.453592kg
+
+print "Now in metric system"
+print "Let's talk about %s." % my_name
+print "He's %f cm tall." % round(my_height * 2.54, 2)
+print "He's %f kilos heavy." % round(my_weight * 0.453592, 2)
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
